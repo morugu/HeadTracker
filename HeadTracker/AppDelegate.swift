86dfbe590c81c5b6a8d2dabd0751138da04a8661
Masaki Fujimoto <masaki.fujimoto@gree.net>
2016-03-16 14:46:29 +0900

    menu bar app

--- HeadTracker/AppDelegate.swift
+++ HeadTracker/AppDelegate.swift
@@ -5,17 +5,29 @@ let log = XCGLogger.defaultInstance()
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
     
     var app:Application!
-
+    var statusItem:NSStatusItem!
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         log.setup(XCGLogger.LogLevel.Debug, showThreadName: true, showLogLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: nil, fileLogLevel: XCGLogger.LogLevel.Debug)
+        
+        let icon = NSImage(named: "statusIcon")
+        icon?.template = true
+        
+        self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+        self.statusItem.image = icon
+        self.statusItem.menu = statusMenu
         
         app = Application()
         app.run()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
+    }
+    
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
     }
 }
