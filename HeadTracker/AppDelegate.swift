4b4bee31644fc790fd3be0189a426ec5616aae89
Masaki Fujimoto <masaki.fujimoto@gree.net>
2016-03-16 10:55:48 +0900

    Initial Commit

---
+++ HeadTracker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HeadTracker
+//
+//  Created by Masaki Fujimoto on 2016/03/16.
+//  Copyright © 2016年 Masaki Fujimoto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
